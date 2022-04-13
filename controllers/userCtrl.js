ffdd5851bccb14aaf3c6cba6d861d09214952136
buihuuthinh456 <swordartonlinethinh@gmail.com>
2022-04-13 19:39:36 +0700

    add feature get all user

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -60,6 +60,15 @@ const userCtrl = {
             return res.status(500).json({msg:err.message})
         }
     },
+    getAllUser: async(req,res)=>{
+        try {
+            const users = await Users.find().select('-password');
+            
+            res.status(200).json(users)
+        } catch (error) {
+            return res.status(500).json({msg:error.message})
+        }
+    }
 }
 
 
--- router/userRouter.js
+++ router/userRouter.js
@@ -2,9 +2,11 @@
 const router = require('express').Router();
 const userCtrl = require('../controllers/userCtrl');
 const auth = require('../middleware/auth');
+const authAdmin = require('../middleware/authAdmin')
 
 router.post('/register',userCtrl.register);
 router.post('/login',userCtrl.login);
 router.get('/infor',auth,userCtrl.getUser);
+router.get('/',auth,authAdmin,userCtrl.getAllUser);
 
 module.exports = router
